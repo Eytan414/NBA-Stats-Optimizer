b3eea050d23e1098fa1544b9995190075d1f9eef
Eytan Ivri <eytanivri@gmail.com>
2021-02-14 04:58:24 +0200

    add popup

---
+++ src/page_action/popup.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        let supportedUrl = tabs[0].url.startsWith('https://www.nba.com/game/');
+            
+        if(supportedUrl){
+            chrome.tabs.sendMessage(tabs[0].id,'ptwSwitch', handleSwitch);
+            }
+        else{
+            document.getElementById('mainPopup').style.display = 'none'; 
+            document.getElementById('fallbackPopup').style.display = 'block';
+        }
+    });
+
+});
+function handleSwitch(trackPlayer){
+    let powerEl = document.getElementById('power');
+    
+    if(trackPlayer === -1)//game over
+        powerEl.classList.add('disabled');
+    else{
+        powerEl.addEventListener('click', togglePlayerTrack);
+        trackPlayer ?
+            powerEl.classList.add('on'):
+            powerEl.classList.remove('on');
+    }
+}
+
+function togglePlayerTrack(ev){
+    let powerBtnClasses = document.getElementById('power').classList;
+    powerBtnClasses.toggle('on');
+    let msg = powerBtnClasses.contains('on') ? "activate" : "deactivate";
+    
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+    });
+    
+    
+}
